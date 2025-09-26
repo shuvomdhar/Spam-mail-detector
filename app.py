c182c4b8cc24c112362292279c2d40c9f3dbd57f
shuvomdhar <shuvomdhar8@gmail.com>
2025-09-26 22:46:36 +0530

    Changes at port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 
@@ -21,4 +22,5 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
